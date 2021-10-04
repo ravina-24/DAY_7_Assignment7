acb8684cfc54a9ee49fc649d22ee92bdb2f0b024
ravina-24 <ravinamaudekar24@gmail.com>
2021-10-04 22:32:04 +0530

    [Ravina] Added : Method to convert decimal to binary.

---
+++ DAY_7_Assignment7/Binary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY_7_Assignment7
+{
+    class Binary
+    {
+        public void findBinary()
+        {
+            int Num;
+            Console.WriteLine("Enter the Decimal Number : ");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            int[] BinaryArray = new int[32];
+
+            int i = 0;
+            while (Num > 0)
+            {
+                BinaryArray[i] = Num % 2;
+                Num = Num / 2;
+                i++;
+
+
+            }
+            int j = 0;
+            Console.WriteLine("Binary Conversion");
+            for (j = i - 1; j >= 0; j--)
+            {
+                
+                Console.Write(+ BinaryArray[j]);
+            }
+
+            Console.ReadLine();
+
+
+
+
+        }
+
+    }
+
+}
+        
